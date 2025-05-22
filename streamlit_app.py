432f3e365e1d585d15265fedd775b032442ed3f2
Thian21 <thomascpanang@gmail.com>
2025-05-22 09:52:26 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,5 @@ import streamlit as st
 st.title("Website Thian")
 st.write("Selamat Datang di web Thomas")
 st.write(
-    "belajar informatika bersama pak tomas"
+    "belajar informatika bersama pak Hendri"
 )
